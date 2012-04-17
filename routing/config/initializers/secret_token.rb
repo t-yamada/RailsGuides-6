beb14bf7e23ffd8ea18c23c35ad951c730635a50
Taku Yamada <t-yamada@groovenauts.jp>
2012-04-17 18:02:53 +0900

    rails new routing

---
+++ routing/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Routing::Application.config.secret_token = '6439ea47721c848364351cab23c74ad2fe6fecbd9998e046b5997d9c833a1c686f94f6f09db5f19db22784fcc96439aa5e04342bc3b91cfe7373c3d6592f79c8'
