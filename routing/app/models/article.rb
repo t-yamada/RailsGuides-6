0afcd22325a5cfdf2c5af811cdf38b766b271532
Taku Yamada <t-yamada@groovenauts.jp>
2012-04-17 19:55:55 +0900

    modified has_one to belongs_to

--- routing/app/models/article.rb
+++ routing/app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
   has_many :comments
-  has_one :user
+  belongs_to :user
 end
--- routing/app/models/comment.rb
+++ routing/app/models/comment.rb
@@ -1,3 +1,3 @@
 class Comment < ActiveRecord::Base
-  has_one :user
+  belongs_to :user
 end
